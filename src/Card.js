8147fe80f4c7d09583ae303e216daff1347b5d57
Robinkphilip <robinphilipp8018@gmail.com>
2022-06-08 19:58:04 +0530

    card commponent added

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img alt="robots" src="https://robohash.org/test?200*200" />
+      <div>
+        <h2>robin k philip</h2>
+        <p>robinkphilip@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+export default Card;
